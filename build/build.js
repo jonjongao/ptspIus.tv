7925281816697665cde64eb55d17e2e54f626788
Jon Gao <jonjon.liho@gmail.com>
2020-11-26 04:46:36 +0800

    optimize images, reset css url, retry another Public Path

--- build/build.js
+++ build/build.js
@@ -2,7 +2,7 @@
 require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
-process.env.PUBLIC_PATH = ''
+process.env.PUBLIC_PATH = './'
 
 const ora = require('ora')
 const rm = require('rimraf')
