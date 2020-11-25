9bbc202b8f72608415f3fd5dddf0d698a18e2b2e
Jon Gao <jonjon.liho@gmail.com>
2020-11-25 22:19:12 +0800

    update Public Path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?
-  process.env.PUBLIC_PATH : '/',
+  process.env.PUBLIC_PATH : '',
   chainWebpack: config => {
     config.module
       .rule("vue")
